9331cbc471b40f8f3ac037258906e0aca0d7213b
mikeyoung83 <mikeby83@gmail.com>
2026-03-28 14:47:01 -0400

    stuff

--- src/content/config.ts
+++ src/content/config.ts
@@ -3,12 +3,17 @@ import { defineCollection, z } from "astro:content";
 
 const portfolio = defineCollection({
   type: "content",
-  schema: z.object({
-    title: z.string(),
-    description: z.string(),
-    image: z.string().default("/images/placeholder.jpg"),
-    publishDate: z.coerce.date(),
-  }),
+  schema: (
+    { image }, // Use the image helper here
+  ) =>
+    z.object({
+      title: z.string(),
+      description: z.string(),
+      // Change this from z.string() to image()
+      image: image(),
+      tags: z.array(z.string()).default([]),
+      publishDate: z.coerce.date(),
+    }),
 });
 
 export const collections = { portfolio };
