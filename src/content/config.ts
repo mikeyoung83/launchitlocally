c8d7d15c1de8e16866e2dab80e9a06462749a1a9
mikeyoung83 <mikeby83@gmail.com>
2026-03-28 14:52:45 -0400

    Update config.ts

--- src/content/config.ts
+++ src/content/config.ts
@@ -4,13 +4,12 @@ import { defineCollection, z } from "astro:content";
 const portfolio = defineCollection({
   type: "content",
   schema: (
-    { image }, // Use the image helper here
+    { image }, // Added the image helper here
   ) =>
     z.object({
       title: z.string(),
       description: z.string(),
-      // Change this from z.string() to image()
-      image: image(),
+      image: image(), // Changed from z.string() to image()
       tags: z.array(z.string()).default([]),
       publishDate: z.coerce.date(),
     }),
